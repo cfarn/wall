a087356dad41e7b6f8ae29596ea2c67f71391b2f
cfarn <58476975+cfarn@users.noreply.github.com>
2022-07-12 12:58:26 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ import Card from './components/Card'
 class App extends React.Component {
   render() {
     return (
-      // hello
+      <div className="container">
+        <h1 className="mt-3 mb-3">The wall</h1>
+        <section className="row gap-3 justify-content-center">
+          {users.map(user => {
+            return (
+              <Card 
+                key = {user.email}
+                name = {user.name}
+                about = {user.about}
+                picture = {user.picture}
+              />
+              
+            )
+          })}
+        </section> 
+      </div>
     )
   }
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 class Card extends React.Component {
   render() {
+    // console.log(this)
     return (
-      // hello
+      <div className="card col-5">
+        <img src={this.props.picture} className="card-img-top" alt="User" />
+        <div className="card-body">
+          <h5 className="card-title">{this.props.name}</h5>
+          <p className="card-text">{this.props.about}</p>
+        </div>
+      </div>
     )
   }
 }
